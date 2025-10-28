fccadb50941562ecc591dc2674ae06e3c8dea4d8
Igor Felipe Silva de Morais <igor.felipe@academico.ifrn.edu.br>
2025-10-28 17:11:29 -0300

    Implementando Validação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -5,16 +5,19 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.ConvidadoRepository;
 import ifrn.pi.eventos.repositories.EventoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/eventos")
@@ -31,10 +34,15 @@ public class EventosController {
 	}
 
 	@PostMapping
-	public String salvar(Evento evento) {
+	public String salvar(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			return form(evento);
+		}
 
 		System.out.println(evento);
 		er.save(evento);
+		attributes.addFlashAttribute("mensagem", "Evento salvo com sucesso!");
 
 		return "redirect:/eventos";
 	}
@@ -132,7 +140,7 @@ public class EventosController {
 	}
 	
 	@GetMapping("/{id}/remover")
-	public String apagarEvento(@PathVariable Long id) {
+	public String apagarEvento(@PathVariable Long id, RedirectAttributes attributes) {
 		
 		Optional<Evento> opt = er.findById(id);
 		
@@ -143,6 +151,7 @@ public class EventosController {
 			
 			cr.deleteAll(convidados);
 			er.delete(evento);
+			attributes.addFlashAttribute("mensagem", "Evento removido com sucesso!");
 		}
 		
 		return "redirect:/eventos";
--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -1,9 +1,16 @@
 package ifrn.pi.eventos.models;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Evento {
@@ -11,10 +18,16 @@ public class Evento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
 	private String nome;
+	@NotBlank
 	private String local;
-	private String data;
-	private String horario;
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate data;
+	@NotNull
+	private LocalTime horario;
 
 	public Long getId() {
 		return id;
@@ -40,19 +53,19 @@ public class Evento {
 		this.local = local;
 	}
 
-	public String getData() {
+	public LocalDate getData() {
 		return data;
 	}
 
-	public void setData(String data) {
+	public void setData(LocalDate data) {
 		this.data = data;
 	}
 
-	public String getHorario() {
+	public LocalTime getHorario() {
 		return horario;
 	}
 
-	public void setHorario(String horario) {
+	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
 
